2c1b87b3902973f0217b9e6d95aaa47421234e0e
DmitrenkoNIkitaa <nikita.dmytrenko@upholding.biz>
2018-10-05 17:56:51 +0300

    Add new conf

---
+++ src/main/java/com/exrates/inout/configuration/OtherConfiguration.java
@@ -0,0 +1,50 @@
+package com.exrates.inout.configuration;
+
+import com.exrates.inout.exceptions.handlers.RestResponseErrorHandler;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.zeromq.ZMQ;
+
+import java.util.Locale;
+
+@Configuration
+public class OtherConfiguration {
+
+
+    @Bean
+    public RestTemplate restTemplate() {
+        HttpClientBuilder b = HttpClientBuilder.create();
+        HttpClient client = b.build();
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setErrorHandler(new RestResponseErrorHandler());
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(client);
+        requestFactory.setConnectionRequestTimeout(25000);
+        requestFactory.setReadTimeout(25000);
+        restTemplate.setRequestFactory(requestFactory);
+        return new RestTemplate();
+    }
+
+    @Bean
+    public ZMQ.Context zmqContext() {
+        return ZMQ.context(1);
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver resolver = new CookieLocaleResolver();
+        resolver.setDefaultLocale(new Locale("en"));
+        resolver.setCookieName("myAppLocaleCookie");
+        resolver.setCookieMaxAge(3600);
+        return resolver;
+    }
+}
+
