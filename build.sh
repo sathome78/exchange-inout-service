1f9dd36dd91e816dcc805f645b95b7dd0eb6feeb
NMaslukov <JavaNoob14679987>
2019-04-23 17:18:05 +0300

    bash script

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ docker kill $(docker ps -q)
 git pull
 mvn clean install
 docker build --build-arg ENVIRONMENT=test -t exrates/exrates-inout-service:test .
-docker run -d -it -p 8090:8080 -v /opt/properties:/opt/properties/ exrates/exrates-inout-service:test
+docker run -p 8090:8080 -v /opt/properties:/opt/properties/ exrates/exrates-inout-service:test
