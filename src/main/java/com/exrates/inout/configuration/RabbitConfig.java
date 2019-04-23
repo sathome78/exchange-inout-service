8ff90f68bfd042d6d953524a2d3f15ef8c4b79c5
NMaslukov <JavaNoob14679987>
2019-04-23 19:42:18 +0300

    refill queue durable

--- src/main/java/com/exrates/inout/configuration/RabbitConfig.java
+++ src/main/java/com/exrates/inout/configuration/RabbitConfig.java
@@ -12,17 +12,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class RabbitConfig  {
 
-    private String host = "172.31.14.1";
-    private int port = 5672;
-    private String username = "angulardemoaccess";
-    private String password = "angulardemoaccess";
     public static final String topicExchangeName = "inout-service";
     public static final String REFILL_QUEUE = "refill";
 
 
     @Bean
     Queue queue() {
-        return new Queue(REFILL_QUEUE, false);
+        return new Queue(REFILL_QUEUE, true);
     }
 
     @Bean
@@ -34,16 +30,6 @@ public class RabbitConfig  {
     Binding binding(Queue queue, TopicExchange exchange) {
         return BindingBuilder.bind(queue).to(exchange).with(REFILL_QUEUE);
     }
-//
-//    @Bean
-//    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
-//                                             MessageListenerAdapter listenerAdapter) {
-//        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-//        container.setConnectionFactory(connectionFactory);
-//        container.setQueueNames(REFILL_QUEUE);
-//        container.setMessageListener(listenerAdapter);
-//        return container;
-//    }
 
     @Bean
     MessageListenerAdapter listenerAdapter(TestRabbitReceiver receiver) {
