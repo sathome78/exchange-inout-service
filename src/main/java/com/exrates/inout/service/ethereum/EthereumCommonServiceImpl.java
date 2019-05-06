ff6a87b01d1516aafb2e5cda6315998c14c8fd54
NMaslukov <JavaNoob14679987>
2019-05-06 23:06:47 +0300

    logging

--- src/main/java/com/exrates/inout/service/ethereum/EthereumCommonServiceImpl.java
+++ src/main/java/com/exrates/inout/service/ethereum/EthereumCommonServiceImpl.java
@@ -296,7 +296,11 @@ public class EthereumCommonServiceImpl implements EthereumCommonService {
                 log.debug(merchantName + " block: " + ethBlock.getBlockNumber());
 
                 /*-------------Tokens--------------*/
+                System.out.println("ethBlock.getTo()" + ethBlock.getTo());
+                System.out.println("ethTokensContext.isContract(ethBlock.getTo())" + ethTokensContext.isContract(ethBlock.getTo()));
+
                 if (ethBlock.getTo() != null && ethTokensContext.isContract(ethBlock.getTo()) && merchantName.equals("Ethereum")) {
+                    System.out.println("first if");
                     ethTokensContext.getByContract(ethBlock.getTo()).tokenTransaction(ethBlock);
                 }
 
