598102894c45462a87bf15f7c31a385c854f9021
gregory.taranik <gregory.taranik@upholding.biz>
2019-06-11 11:44:19 +0300

    up

--- src/main/java/com/exrates/inout/properties/models/ArkProperty.java
+++ src/main/java/com/exrates/inout/properties/models/ArkProperty.java
@@ -1,14 +1,8 @@
 package com.exrates.inout.properties.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@Builder(builderClassName = "Builder", toBuilder = true)
-@NoArgsConstructor
-@AllArgsConstructor
 public class ArkProperty extends LiskProperty {
     private ArkNode arcNode;
 }
