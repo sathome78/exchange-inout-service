954d7e255a943443329cf40dbc90e476d75fec3b
vlad.dziubak <vlad.dziubak@upholding.biz>
2019-06-24 15:34:31 +0300

    Fix log for tcp socket (added active profile to json for each log)

---
+++ src/main/java/com/exrates/inout/configuration/LoggingListener.java
@@ -0,0 +1,26 @@
+package com.exrates.inout.configuration;
+
+import org.apache.logging.log4j.ThreadContext;
+import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
+import org.springframework.boot.context.logging.LoggingApplicationListener;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.core.Ordered;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+public class LoggingListener implements ApplicationListener, Ordered {
+    @Override
+    public int getOrder() {
+        return LoggingApplicationListener.DEFAULT_ORDER - 1;
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+        if (event instanceof ApplicationEnvironmentPreparedEvent) {
+            ConfigurableEnvironment environment = ((ApplicationEnvironmentPreparedEvent) event).getEnvironment();
+
+            String activeProfile = environment.getProperty("spring.profiles.active");
+            ThreadContext.put("spring.profiles.active", activeProfile);
+        }
+    }
+}
