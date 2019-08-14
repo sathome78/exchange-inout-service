f4eeee56978feca6195f10937192d5d1de8f704e
gregory.taranik <gregory.taranik@upholding.biz>
2019-08-14 17:21:34 +0300

    temp

---
+++ src/main/java/com/exrates/inout/properties/models/BinanceProperty.java
@@ -0,0 +1,14 @@
+package com.exrates.inout.properties.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder(builderClassName = "Builder", toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class BinanceProperty {
+    private String str;
+}
